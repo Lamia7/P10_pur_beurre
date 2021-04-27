f9b82d7148b4ebd018506a284d447f50b3a7d585
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-04-27 23:32:47 +0200

    Delete typo

--- tests/users/functional_tests/test_authentication.py
+++ tests/users/functional_tests/test_authentication.py
@@ -2,16 +2,13 @@
 Functional test using Selenium to test the behavior
 Check README file to to launch this test.
 """
-import os
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 
 from purbeurre.settings import BASE_DIR
 
 
-# SUPPR
 firefox_options = webdriver.FirefoxOptions()
 firefox_options.headless = True
 
@@ -20,21 +17,7 @@ class SeleniumRegisterTest(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        # firefox_options = Options()
-        # firefox_options.add_argument("--headless")
-        # geckodriver = os.getcwd() + "/geckodriver"
-        # print(geckodriver)
-        # print(f"BASE DIR: {BASE_DIR}")
-        # cls.driver = webdriver.Firefox(
-        #    executable_path=geckodriver, options=firefox_options
-        # )
-        # cls.driver.implicitly_wait(10)
-
-        # SUPPR
         geckodriver = str(BASE_DIR / "webdrivers" / "geckodriver")
-
-        print(geckodriver)
-        print(f"BASE DIR: {BASE_DIR}")
         cls.driver = webdriver.Firefox(
             executable_path=geckodriver, options=firefox_options
         )
@@ -50,7 +33,6 @@ class SeleniumRegisterTest(StaticLiveServerTestCase):
         """Launches the functional test for the registration
         and automatical login feature"""
         # Access register page and fill fields
-        # self.selenium.get("%s%s" % (self.live_server_url, "/register/"))
         self.driver.get("%s%s" % (self.live_server_url, "/register/"))
         email_input = self.driver.find_element_by_name("email")
         email_input.send_keys("usertest1@email.com")
