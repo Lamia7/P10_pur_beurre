d1af0760a28606e00a66daff5279a861ba4db58e
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-04-29 12:40:10 +0200

    Add environment condition

--- purbeurre/settings/__init__.py
+++ purbeurre/settings/__init__.py
@@ -186,14 +186,15 @@ then to the page they tried to access to (@decorators in views)"""
 LOGIN_URL = "login"
 
 # Sentry configuration
-sentry_sdk.init(
-    dsn="https://fd036ed83aec4c1d8d85d5129bb76c37@o592289.ingest.sentry.io/5740647",
-    integrations=[DjangoIntegration()],
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True,
-)
+if ENV == "production":
+    sentry_sdk.init(
+        dsn="https://fd036ed83aec4c1d8d85d5129bb76c37@o592289.ingest.sentry.io/5740647",
+        integrations=[DjangoIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+    )
